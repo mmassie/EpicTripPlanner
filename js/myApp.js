09167dd5762a4011c0401c72440efc7eccdd0178
Mike Massie <mike@michaelmassie.com>
2016-03-05 11:18:24 -0600

    Initial Upload

---
+++ js/myApp.js
@@ -0,0 +1,11 @@
+angular
+  .module('myApp',['ngMaterial'])
+  .controller('GridCtrl', ['$scope', function ($scope) {
+    $scope.grid = [[1,2],[3,4],[5,6]];
+  }]);
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved. 
+Use of this source code is governed by an MIT-style license that can be in foundin the LICENSE file at http://material.angularjs.org/license.
+*/
